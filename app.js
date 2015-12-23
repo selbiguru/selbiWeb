502641bf3ea5e55bc7c1e92b0873fe86eda309fa
jordanharris <jordanburrows@gmail.com>
2015-12-22 22:08:27 -0800

    Updating error page not found.  Adding forgot password view and js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-      message: err.message,
+      message: "Page not found",
       error: err
     });
   });
@@ -51,7 +51,7 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
-    message: err.message,
+    message: "Uh oh! This page does not exist on Selbi",
     error: {}
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET reset password page. */
+router.get('/resetpassword', function(req, res, next) {
+  res.render('forgotPassword', { title: 'Express' });
+});
+
 module.exports = router;
