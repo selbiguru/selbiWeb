670a4e3f4fbf427efe35d23a1b215a75840044f0
jordanharris <jordanburrows@gmail.com>
2016-03-05 18:39:12 -0800

    updating selbi home page with privacy policy and user agreement.  Added title change on Selbi

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Selbi' });
 });
 
 /* GET reset password page. */
 router.get('/resetpassword/:anything', function(req, res, next) {
-  res.render('forgotPassword', { title: 'Express' });
+  res.render('forgotPassword', { title: 'Selbi' });
 });
 
 /* GET successful reset password page. */
 router.get('/success', function(req, res, next) {
-  res.render('passwordSuccess', { title: 'Express' });
+  res.render('passwordSuccess', { title: 'Selbi' });
 });
 
 module.exports = router;
